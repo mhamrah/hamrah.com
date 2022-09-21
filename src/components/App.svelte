02733053fe9fea2472ff87a0133ad6058803d14f
Mike Hamrah <m@flowcode.com>
2022-09-20 22:24:02 -0400

    login

---
+++ src/components/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Auth0Client, User } from "@auth0/auth0-spa-js";
+
+  export let client: Auth0Client;
+
+  export let user: User | undefined;
+
+  console.log(client);
+
+  const logout = () => {
+    client.logout({
+      returnTo: "http://localhost:3000/",
+    });
+  };
+</script>
+
+{#if user}
+  {user.family_name} {user.email} <button on:click={logout}>logout</button>
+{:else}{/if}
---
+++ src/components/Login.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import createAuth0Client, { Auth0Client, User } from "@auth0/auth0-spa-js";
+  import App from "./App.svelte";
+
+  let client: Auth0Client;
+
+  let auth0 = createAuth0Client({
+    domain: "hamrah.auth0.com",
+    client_id: "5Hj47gq7sXbtb9xYUtBOwHZ7n4t2Pf3S",
+    cacheLocation: "localstorage",
+  });
+
+  let user: User | undefined;
+
+  let check = async () => {
+    client = await auth0;
+    user = await client.getUser();
+    console.log("u", user);
+  };
+
+  let login = async () => {
+    await client.loginWithPopup({
+      redirect_uri: "http://localhost:3000/",
+    });
+    user = await client.getUser();
+    console.log("login finished", user);
+  };
+</script>
+
+{#await check()}
+  <p>...waiting</p>
+{:then u}
+  {#if user}
+    <App {client} {user} />
+  {:else}
+    <button on:click={login}>Login</button>
+  {/if}
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
