45d9524390531b2b5fde33384889a228caa1a68d
Mike Hamrah <m@flowcode.com>
2022-09-22 21:17:25 -0400

    try without path.js

--- functions/graphql.ts
+++ functions/graphql.ts
@@ -1,26 +1,26 @@
-// import graphQLPlugin from "@cloudflare/pages-plugin-graphql";
-// import {
-//   graphql,
-//   GraphQLSchema,
-//   GraphQLObjectType,
-//   GraphQLString,
-// } from "graphql";
+import graphQLPlugin from "@cloudflare/pages-plugin-graphql";
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
 
-// const schema = new GraphQLSchema({
-//   query: new GraphQLObjectType({
-//     name: "RootQueryType",
-//     fields: {
-//       hello: {
-//         type: GraphQLString,
-//         resolve() {
-//           return "Hello, world!";
-//         },
-//       },
-//     },
-//   }),
-// });
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return "Hello, world!";
+        },
+      },
+    },
+  }),
+});
 
-// export const onRequest: PagesFunction = graphQLPlugin({
-//   schema,
-//   graphql,
-// });
+export const onRequest: PagesFunction = graphQLPlugin({
+  schema,
+  graphql,
+});
