2ba9faf6072ac3d53d5dce819f36f8b9c94bac87
Mike Hamrah <m@flowcode.com>
2022-09-25 15:55:07 -0400

    remove functions and use worker api

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,22 +4,28 @@
   export let user: User | undefined;
   export let getToken: () => Promise<string>;
 
+  let todos = [];
+
   const callApi = async () => {
     const token = await getToken();
-    const result = await fetch("/api", {
+    const result = await fetch("https://api.hamrah.com/todos", {
       method: "GET",
       headers: {
         Authorization: "Bearer " + token,
       },
     });
-    const data = await result.json();
-    console.log(data);
+    todos = await result.json();
+    console.log(todos);
   };
 </script>
 
 {#if user}
   {user.family_name}
-  {user.email} <button on:click={callApi}>token</button>
+  {user.email} <button on:click={callApi}>get todos</button>
+
+  {#each todos as todo (todo.id)}
+    {todo.id} - {todo.name} - {todo.created_at}
+  {/each}
 {:else}
   No user
 {/if}
